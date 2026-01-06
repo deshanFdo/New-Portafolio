ffc77440fe31d40fc3f147dafd2d3015eb2d037d
deshanFdo <deshanfernando67@gmail.com>
2026-01-06 11:52:59 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/New-Portafolio',
-  assetPrefix: '/New-Portafolio/',
+  basePath: isProd ? '/New-Portafolio' : '',
+  assetPrefix: isProd ? '/New-Portafolio/' : '',
   trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
